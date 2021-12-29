ec883a608adb0ca8b0094c10cb22bc3dd1a61ac9
Marc Castro <marcjgcastro@gmail.com>
2021-12-29 00:10:51 -0800

    created css and grid for template. will work on js tomorrow

---
+++ Sorting-Visualizer/js/newArray.js
@@ -0,0 +1,10 @@
+function newArray(param) {
+  int parameter = param;
+  for(int i = 0; i<=parameter; i++) {
+    var length = randomInt(100);
+  }
+}
+
+function randomInt(max) {
+  return Math.floor(Math.random() * max);
+}
